cc3ca75196d19a3100bd183c29559aa9fcdde31b
jeremykuang@foxmail.com <jeremykuang@foxmail.com>
2018-08-28 16:39:46 +0800

    封装完成

--- src/common.php
+++ src/common.php
@@ -1,33 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: JeremyK
- * Date: 2018/08/27
- * Time: 21:23
- */
-
-if (!function_exists('runtime_path')) {
-
-
-    /**
-     * get path to runtime cache
-     * @param string $path
-     * @return string
-     */
-    function runtime_path($path='')
-    {
-        return  RUNTIME_PATH.($path ? DIRECTORY_SEPARATOR.$path : $path);
-
-    }
-
-}
-
 
 if (!function_exists('clean')) {
-    function clean($dirty, $config = null)
-    {
-        
-        return (new Jeristiano\Pruifier())->clean($dirty, $config);
-      //  return app('purifier')->clean($dirty, $config);
-    }
+	function clean($dirty, $config = null) {
+		return (new Jeristiano\Purifier(new \think\Config()))->clean($dirty, $config);
+	}
 }
